e93e7b85238e484b989dbc7c7c881fc0ce41eebc
user <user@DESKTOP-A260DQJ>
2017-06-08 13:14:44 +0300

    Added more code to server side of static pages.

---
+++ DigitalIsraelFund_System/Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using DigitalIsraelFund_System.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DigitalIsraelFund_System.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        [HttpPost]
+        public JsonResult PageUpdate(string page, string title, string html)
+        {
+            this.SavePage(page, new StaticPage() { Title = title, HtmlContent = html });
+            return Json(new { Success = true }, JsonRequestBehavior.AllowGet);
+        }
+
+        [NonAction]
+        private void SavePage(string filename, StaticPage page)
+        {
+            var dataFile = Server.MapPath("~/App_Data/Pages/" + filename + ".json");
+            string json = page.GetJson();
+            System.IO.File.WriteAllText(@dataFile, json);
+        }
+
+
+    }
+}
---
+++ DigitalIsraelFund_System/Controllers/GovExpController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DigitalIsraelFund_System.Controllers
+{
+    public class GovExpController : Controller
+    {
+        // GET: GovExp
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
