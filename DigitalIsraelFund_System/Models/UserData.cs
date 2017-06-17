9c3c9cc0e7061552c0e90e60fcdf378be6fb14e1
user <user@DESKTOP-A260DQJ>
2017-06-17 14:31:20 +0300

    Moved the validation js function to a seperate file.

--- DigitalIsraelFund_System/Models/UserData.cs
+++ DigitalIsraelFund_System/Models/UserData.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace DigitalIsraelFund_System.Models
+﻿namespace DigitalIsraelFund_System.Models
 {
     public class UserData
     {
---
+++ DigitalIsraelFund_System/Scripts/validation.js
@@ -0,0 +1,32 @@
+﻿function validateText(id, type) {
+    validate(document.getElementById(id).innerText, type, id);
+}
+
+function validateInput(id, type) {
+    validate(document.getElementById(id).value, type, id);
+}
+
+function validate(value, type, id) {
+    var data = {
+        "value": value,
+        "type": type
+    };
+    $.ajax({
+        url: "/Request/ValidateField",
+        type: "POST",
+        data: JSON.stringify(data),
+        dataType: "json",
+        contentType: "application/json",
+        success: function (response) {
+            if (response.Success) {
+                document.getElementById(id).style.borderRightColor = '#38ACEC';
+            }
+            else {
+                document.getElementById(id).style.borderRightColor = '#ff0000';
+            }
+        },
+        error: function () {
+            alert('בעיה בשרת');
+        }
+    });
+}
